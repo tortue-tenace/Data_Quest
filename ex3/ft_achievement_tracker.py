540ed91131c73cfcba8aff160c76c45e710741d3
thattal <thattal@f1br10s5.paris.42.school>
2026-06-08 21:38:48 +0200

    save

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,2 +1,15 @@
-import random
+import random as rd
 
+def gen_player_achievements():
+    achievements = ["Crafting genius", "World savior", "Master Explorer", "Strategist", "Minimalist", 
+                    "Infected slayer", "Collector Boss", "Don't touch me", "How i met your father"]
+    jason = set(rd.sample(achievements, k=rd.randint(0,len(achievements))))
+    louis = set(rd.sample(achievements, k=rd.randint(0,len(achievements))))
+    thomas = set(rd.sample(achievements, k=rd.randint(0,len(achievements))))
+    booba = set(rd.sample(achievements, k=rd.randint(0,len(achievements))))
+    print(f"Player Jason: {jason}")
+    print(f"Player Louis: {louis}")
+    print(f"Player Thomas: {thomas}")
+    print(f"Player Booba: {booba}")
+
+gen_player_achievements()
